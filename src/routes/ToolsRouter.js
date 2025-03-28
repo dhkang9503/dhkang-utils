2fbf31dde9a3ee68a7a75d942b0f6f74116666f2
DonghyunKang <88758902+dhkang9503@users.noreply.github.com>
2025-03-28 14:51:37 +0900

    Update ToolsRouter.js

--- src/routes/ToolsRouter.js
+++ src/routes/ToolsRouter.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import PomodoroPage from '../tools/pomodoro/PomodoroPage';
 import UnitConverterPage from '../tools/unit-converter/UnitConverterPage';
+import JsonFormatterPage from '../tools/json-formatter/JsonFormatterPage';
 
 const ToolsRouter = () => {
   return (
     <Routes>
       <Route path="pomodoro" element={<PomodoroPage />} />
       <Route path="unit-converter" element={<UnitConverterPage />} />
+      <Route path="json-formatter" element={<JsonFormatterPage />} />
     </Routes>
   );
 };
