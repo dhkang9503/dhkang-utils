11578c177416b96deeaf07f428fe941c3ec5fc9d
DonghyunKang <88758902+dhkang9503@users.noreply.github.com>
2025-03-28 10:41:51 +0900

    Update PomodoroPage.js

--- src/tools/pomodoro/PomodoroPage.js
+++ src/tools/pomodoro/PomodoroPage.js
@@ -56,7 +56,6 @@ const PomodoroPage = () => {
   return (
     <>
     <head>
-    <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZNVHWFSQFW"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
