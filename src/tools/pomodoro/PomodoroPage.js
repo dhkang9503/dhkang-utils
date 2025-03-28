7ac20a9a5ad63f038c2757008d2fc4a03735176f
DonghyunKang <88758902+dhkang9503@users.noreply.github.com>
2025-03-28 10:40:06 +0900

    Update PomodoroPage.js

--- src/tools/pomodoro/PomodoroPage.js
+++ src/tools/pomodoro/PomodoroPage.js
@@ -55,6 +55,17 @@ const PomodoroPage = () => {
 
   return (
     <>
+    <head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZNVHWFSQFW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZNVHWFSQFW');
+</script>
+    </head>
     <Helmet>
       <title>포모도로 타이머 | MyTools</title>
       <meta name="description" content="집중력을 높이는 포모도로 타이머를 사용해보세요!" />
