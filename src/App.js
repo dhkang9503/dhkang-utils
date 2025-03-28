d34b1d00479f1af9741d189e7827f483907fb694
DonghyunKang <88758902+dhkang9503@users.noreply.github.com>
2025-03-28 13:33:52 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // App.tsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ToolsRouter from './routes/ToolsRouter';
+import HomePage from '../pages/HomePage';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/tools/*" element={<ToolsRouter />} />
         {/* 홈이나 기타 페이지도 여기에 */}
       </Routes>
