6e866fac52adc61499439d12e047ac8017f68e90
DonghyunKang <88758902+dhkang9503@users.noreply.github.com>
2025-03-31 10:53:00 +0900

    Update LoremIpsumPage.js

--- src/tools/lorem-ipsum/LoremIpsumPage.js
+++ src/tools/lorem-ipsum/LoremIpsumPage.js
@@ -24,9 +24,9 @@ const LoremIpsumPage = () => {
   return (
     <>
       <Helmet>
-        <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-0QMLB53J2V"></script>
-<script>{`
+<script>
+  {`
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
